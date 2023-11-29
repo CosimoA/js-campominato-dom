c3a6c6da42f600ba187365148102395920380c47
CosimoA <cosimo.archip@gmail.com>
2023-11-29 17:19:25 +0100

    FEATURE: create function bomb generator & Array's

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 /* 
 Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco.
-Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba.
 In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
 Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
 La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
@@ -10,13 +10,12 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 // SELEZIONO ELEMENTI UTILI IN HTML & VARIABILI PREIMPOSTATE
 const bottone = document.getElementById("btn");
 const container = document.getElementById("general-container");
+// CREO ARRAY PER LE 16 BOMBE
+const bombe = creaArrRandomNum(1, 100, 16);
+console.log(bombe);
+// CREO ARRAY PER LA CONTA DEI PUNTI
+const punti = [];
 
-// FUNZIONE CREA ELEMENTO
-function creaElemento(tag, classe){
-    const elemento = document.createElement(tag);
-    elemento.classList.add(classe);
-    return elemento;
-};
 
 // AL CLICK SUL BOTTONE INSERISCO ELEMENTO SQUARE ALLINTERNO DEL CONTAINER
 bottone.addEventListener("click", 
@@ -32,7 +31,7 @@ bottone.addEventListener("click",
             quadrato.addEventListener("click",
                 function(){
                     // AGGIUNGO ALL'ELEMENTO CLICCATO LA CLASSE
-                    quadrato.classList.add("bg-blu");
+                    quadrato.classList.add("bg-salvo");
                     // OUTPUT IN CONSOLE DEL NUMERO CLICCATO
                     console.log("Hai selezionato la cella " + i);  
                 }
@@ -40,6 +39,33 @@ bottone.addEventListener("click",
         }
     }
 );
+
+// ---------------------FUNZIONI---------------------
+
+// FUNZIONE CREA ELEMENTO
+function creaElemento(tag, classe){
+    const elemento = document.createElement(tag);
+    elemento.classList.add(classe);
+    return elemento;
+}
+
+// FUNZIONE CHE GENERA NUMERO RANDOM IN UN RANGE
+function creaNumRandomMinMax(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// FUNZIONE CHE CREA UN ARRAY CON NUMERI IN ORDINE CASUALE IN UN RANGE
+function creaArrRandomNum(minNum, maxNumm, lunghezza){
+    const arrayRandom = [];
+    while (arrayRandom.length < lunghezza){
+        let numRandom = creaNumRandomMinMax(minNum, maxNumm);
+        // SE IL NUMERO NON è PRESENTE NELL'ARRAY LO PUSHO
+        if (!arrayRandom.includes(numRandom)){
+            arrayRandom.push(numRandom);
+        }
+    }
+    return arrayRandom;
+}
 
 
     
